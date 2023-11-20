2dc4dec2b3f0c90fe1c24a61915addfdb180c692
hgalioulline <8976876+hgalioulline@users.noreply.github.com>
2023-11-20 22:56:36 +0100

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,5 +1,6 @@
-const puppeteer = require("puppeteer");
-require("dotenv").config();
+import puppeteer from 'puppeteer';
+import fetch from 'node-fetch';
+import('dotenv/config');
 
 const scrapeLogic = async (res) => {
   const browser = await puppeteer.launch({
@@ -16,36 +17,64 @@ const scrapeLogic = async (res) => {
   });
   try {
     const page = await browser.newPage();
+    await page.goto('https://www.tbooking.ch/de/book/3293-3788', { waitUntil: 'networkidle2' });
 
-    await page.goto("https://developer.chrome.com/");
+    // Selector for the element you want to click
+    const selector = '#container-row > div > div > router-view > div > div:nth-child(3) > div > treatment-view > article';
+    await page.waitForSelector(selector);
+    await page.click(selector);
+    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    await page.waitForSelector('#calendar-0 > calendar > div.au-target.calendar-container > div.table-responsive.calendar > table > tbody > tr:nth-child(1)');
+    
+    // Execute script in the page context
+    const totalTimings = await page.evaluate(() => {
+        let topRow = document.querySelector('#calendar-0 > calendar > div.au-target.calendar-container > div.table-responsive.calendar > table > tbody > tr:nth-child(1)');
+        let bottomRow = document.querySelector('#calendar-0 > calendar > div.au-target.calendar-container > div.table-responsive.calendar > table > tbody > tr:nth-child(2)');
+        let dates = document.querySelector('#calendar-0 > calendar > div.au-target.calendar-container > div.table-responsive.calendar > table > thead > tr.cal-day-row');
+        let totalTimings = "";
+        for (let i = 1; i < topRow.childNodes.length - 2; i++) {
+            let timings = "";
+            let optionsMorning = topRow.childNodes[i].childNodes
+            let optionsAfternoon = bottomRow.childNodes[i].childNodes
+            for (let j = 1; j < optionsMorning.length - 2; j++) {
+                timings += optionsMorning[j].innerText + "; ";
+            }
+            for (let j = 1; j < optionsAfternoon.length - 2; j++) {
+                timings += optionsAfternoon[j].innerText + "; ";
+            }
+            if (optionsMorning.length > 3 || optionsAfternoon.length > 3) {
+                timings = dates.childNodes[i].innerText + " options: " + timings + "\n";
+            }
+            totalTimings += timings;
+        }
+        return totalTimings;
+    });
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
+    // Trigger the Webhook
+    if (totalTimings.length > 0) {
+        const event = "Physio";
+        const key = "sAa4A7qLjqvIMKk2LsO2E";
+        const url = `https://maker.ifttt.com/trigger/${event}/json/with/key/${key}`;
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+        const data = JSON.stringify({ "timings": totalTimings });
 
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+        const response = await fetch(url, {
+            method: "POST",
+            body: data,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
 
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+        if (response.ok) {
+            console.log("Webhook triggered successfully!");
+        } else {
+            console.log("Error triggering webhook: ", response.statusText);
+        }
+    }
 
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
-  } catch (e) {
-    console.error(e);
-    res.send(`Something went wrong while running Puppeteer: ${e}`);
-  } finally {
     await browser.close();
   }
 };
 
-module.exports = { scrapeLogic };
+export { scrapeLogic };
